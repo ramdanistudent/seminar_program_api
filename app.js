1043f5d4b09bb7581022d3e55093df1502a7ac0f
ramdanigit <ramdanicareer@gmail.com>
2019-09-18 12:46:54 +0700

    add gte & post

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const joi = require("@hapi/joi");
 const app = express();
+
+app.use(express.json());
+
+const barangs = [
+  { id: 1, name: "printer" },
+  { id: 2, name: "laptop" },
+  { id: 3, name: "monitor" }
+];
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -9,4 +18,24 @@ app.get("/api/barang", (req, res) => {
   res.send("barang api");
 });
 
-app.listen(3001, () => console.log("port 3001"));
+app.post("/api/barang", (req, res) => {
+  const barang = {
+    id: barangs.length + 1,
+    name: req.body.name
+  };
+  barangs.push(barang);
+  res.send(barang);
+});
+
+app.get("/api/barang/:id", (req, res) => {
+  const barang = barangs.find(c => c.id === parseInt(req.params.id));
+  if (!barang) res.status(404).send("not found");
+  res.send(barang);
+});
+
+// app.get("/api/barang/:id", (req, res) => {
+//   res.send(req.params.id);
+// });
+//PORT
+const port = process.env.port || 3001;
+app.listen(port, () => console.log(`server started at port ${port}`));
