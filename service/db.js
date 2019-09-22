b07c61208fa73632f65e88872a15f0cd3c24be4c
ramdanistudent <ramdanistudent@gmail.com>
2019-09-22 18:36:39 +0700

    model

---
+++ service/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/griyasisapp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("conected to mongodb"))
+  .catch(err => console.error("gagal connect", err));
